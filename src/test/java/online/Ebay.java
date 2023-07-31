dbd08e3f4f93a548a5bfedce117324f709b1f72f
yogitashinde88 <yogitashinde88gmail.com>
2023-07-31 15:09:11 +0200

    [Yogitashinde88] completed ebay task

---
+++ src/test/java/online/Ebay.java
@@ -0,0 +1,22 @@
+package online;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Ebay {
+  @Test
+  public void ebayTest() 
+  {
+	  
+	  WebDriver driver= new ChromeDriver();
+	  driver.get("https://www.ebay.com/");
+	  driver.manage().window().maximize();
+	  driver.quit();
+	  
+	  
+	  
+  }
+}
+  
